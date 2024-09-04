3078f3ccd87dacf8e5f1ed32a27f8e6c67a642e3
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-09-03 22:53:08 -0300

    adicionando exemplo inicial com treamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def hello_word():
+    return "Eu sou um vencedor e ja conquistei!"
+
+def main():
+    st.write(hello_word())
+    return
+
+if __name__ == "__main__":
+    main()
